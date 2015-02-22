b1baa453b293abc007fb4ae1d88d7a81ef419763
tsuyoshi.ogawa <mobile.chime@gmail.com>
2015-02-22 14:59:53 +0900

    use file store on development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.cache_store = :file_store, "#{config.root}/tmp/cache_store/",{
+    namespace:   "development",
+    expires_in:  5.minutes,
+    compression: false }
 end
